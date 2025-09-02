1ae31e45de40763391560268004e31189299f5e0
Brian <brian.noh95@gmail.com>
2025-09-01 22:48:31 -0400

    create LessonConfiguration for ApplicationDbContext  model builder config

---
+++ src/ContextExplained.Infrastructure/Configurations/LessonConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using ContextExplained.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ContextExplained.Infrastructure.Configurations;
+
+public class LessonConfiguration : IEntityTypeConfiguration<Lesson>
+{
+    public void Configure(EntityTypeBuilder<Lesson> builder)
+    {
+        builder.Property(l => l.Id).HasDefaultValueSql("NEWSEQUENTIALID()");
+
+        builder.Property(l => l.Book).IsRequired();
+        builder.Property(l => l.Chapter).IsRequired();
+
+        builder.OwnsOne(l => l.VerseRange, vr =>
+        {
+            vr.Property(v => v.Start).HasColumnName("VerseStart").IsRequired();
+            vr.Property(v => v.End).HasColumnName("VerseEnd").IsRequired();
+        });
+
+        builder.Property(l => l.Passage).IsRequired();
+        builder.Property(l => l.Context).IsRequired();
+        builder.Property(l => l.Themes).IsRequired();
+        builder.Property(l => l.Reflection).IsRequired();
+    }
+}
