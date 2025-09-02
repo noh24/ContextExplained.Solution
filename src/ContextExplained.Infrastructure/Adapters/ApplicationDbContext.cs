f42ed422e1d6c65197a03c0bb892cf2fc0e1abba
Brian <brian.noh95@gmail.com>
2025-09-01 22:47:49 -0400

    create ApplicationDbContext interface and adapter

---
+++ src/ContextExplained.Infrastructure/Adapters/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ContextExplained.Core.Entities;
+using ContextExplained.Infrastructure.Configurations;
+using ContextExplained.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContextExplained.Infrastructure.Adapters;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Lesson> Lessons => Set<Lesson>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new LessonConfiguration());
+    }
+}
+
---
+++ src/ContextExplained.Infrastructure/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using ContextExplained.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContextExplained.Infrastructure.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<Lesson> Lessons { get; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
