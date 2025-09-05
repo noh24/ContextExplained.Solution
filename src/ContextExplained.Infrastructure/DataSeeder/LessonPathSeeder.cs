d04804194e9a24f16bcc7df7c44ac3e7fa279e77
Brian <brian.noh95@gmail.com>
2025-09-05 01:02:10 -0400

    create method to seed Lesson Path data

---
+++ src/ContextExplained.Infrastructure/DataSeeder/LessonPathSeeder.cs
@@ -0,0 +1,24 @@
+﻿using ContextExplained.Core.Entities;
+using ContextExplained.Core.ValueObjects;
+using ContextExplained.Infrastructure.Adapters;
+
+namespace ContextExplained.Infrastructure.DataSeeder;
+
+public static class LessonPathSeeder
+{
+    public static async Task SeedAsync(ApplicationDbContext db)
+    {
+        if (!db.Set<LessonPath>().Any())
+        {
+            var chronologicalPath = new List<LessonPath>();
+            foreach (var (book, sequence) in LessonPathsArchive.Chronological)
+            {
+                chronologicalPath.Add(new LessonPath(LessonPathType.Chronological, book, sequence));
+            }
+
+            db.AddRange(chronologicalPath);
+            await db.SaveChangesAsync();
+        }
+    }
+
+}
