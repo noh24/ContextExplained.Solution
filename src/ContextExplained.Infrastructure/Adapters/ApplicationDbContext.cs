a006758e189420078bbb75f9f76cb9ae7777a4e8
Brian <brian.noh95@gmail.com>
2025-09-05 01:01:34 -0400

    add LessonPath to ApplicationDbContext

--- src/ContextExplained.Infrastructure/Adapters/ApplicationDbContext.cs
+++ src/ContextExplained.Infrastructure/Adapters/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using ContextExplained.Core.Entities;
+using ContextExplained.Core.ValueObjects;
 using ContextExplained.Infrastructure.Configurations;
 using ContextExplained.Infrastructure.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -10,10 +11,12 @@ public class ApplicationDbContext : DbContext, IApplicationDbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
     public DbSet<Lesson> Lessons => Set<Lesson>();
+    public DbSet<LessonPath> LessonPaths => Set<LessonPath>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new LessonConfiguration());
+        modelBuilder.ApplyConfiguration(new LessonPathConfiguration());
     }
 }
 
