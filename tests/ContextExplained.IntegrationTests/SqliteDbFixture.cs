a5278e6062df459b99f677fd88c440746dedff65
Brian <brian.noh95@gmail.com>
2025-09-07 23:49:49 -0400

    create a SQLite fixture class for integration testing

---
+++ tests/ContextExplained.IntegrationTests/SqliteDbFixture.cs
@@ -0,0 +1,30 @@
+﻿using ContextExplained.Infrastructure.Adapters;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContextExplained.IntegrationTests;
+
+public class SqliteDbFixture : IDisposable
+{
+    public ApplicationDbContext Context { get; }
+    private readonly SqliteConnection _connection;
+
+    public SqliteDbFixture()
+    {
+        _connection = new SqliteConnection("Filename=:memory:");
+        _connection.Open();
+
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseSqlite(_connection)
+            .Options;
+
+        Context = new ApplicationDbContext(options);
+        Context.Database.EnsureCreated();
+    }
+
+    public void Dispose()
+    {
+        Context.Dispose();
+        _connection.Dispose();
+    }
+}
