148b01672c3df22718faf29986b0ede73a05abb4
Brian <brian.noh95@gmail.com>
2025-09-08 01:33:32 -0400

    move IntegrationTest and Test.Common files to Test project and install package dependencies

---
+++ tests/ContextExplained.Tests/Infrastructure/LessonRepositoryIntegrationTests.cs
@@ -0,0 +1,76 @@
+﻿using ContextExplained.Services.DTOs;
+using ContextExplained.Core.Entities;
+using ContextExplained.Core.ValueObjects;
+using ContextExplained.Infrastructure.Adapters;
+using ContextExplained.Infrastructure.Repositories;
+using ContextExplained.Infrastructure.DataSeeder;
+using ContextExplained.Tests.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContextExplained.IntegrationTests;
+
+public class LessonRepositoryIntegrationTests : IClassFixture<SqliteDbFixture>
+{
+    private readonly ApplicationDbContext _dbContext;
+    private readonly LessonRepository _repository;
+    private readonly SqliteDbFixture _fixture;
+
+    public LessonRepositoryIntegrationTests(SqliteDbFixture fixture)
+    {
+        _fixture = fixture;
+        _dbContext = fixture.Context;
+        _repository = new LessonRepository(_dbContext);
+    }
+
+    private async Task SeedLessonPath()
+    {
+        await _fixture.SeedAsync(LessonPathSeeder.SeedAsync);
+    }
+    [Fact]
+    public async Task TestSeeding()
+    {
+        await SeedLessonPath();
+
+        var lessonPaths = await _dbContext.LessonPaths.ToListAsync();
+        Assert.NotEmpty(lessonPaths);
+        
+        var genesisPath = lessonPaths.FirstOrDefault(lp => lp.Book.Contains("Genesis"));
+
+        Assert.Equal("Genesis", genesisPath?.Book);
+        Assert.Equal(1, genesisPath?.Sequence);
+    }
+
+    [Fact]
+    public async Task GetAllLessonsAsync_ReturnsAllLessons()
+    {
+        var lessons = FakeLessonGenerator.CreateMany(2).ToList();
+
+        await _dbContext.Lessons.AddRangeAsync(lessons);
+        await _dbContext.SaveChangesAsync();
+        await SeedLessonPath();
+
+        var getLessons = await _repository.GetAllLessonsAsync();
+
+        Assert.Equal(2, getLessons.Count());
+        Assert.Contains(getLessons, l => l.Passage == lessons[0].Passage);
+        Assert.Contains(getLessons, l => l.Passage == lessons[1].Passage);
+    }
+
+    [Fact]
+    public async Task GetAllLessonsAsync_ReturnsChronologically_WhenSameBookAndChapter()
+    {
+        var lesson1 = FakeLessonGenerator.CreateChronological("Genesis", 1, new VerseRange(1, 5));
+        var lesson2 = FakeLessonGenerator.CreateChronological("Genesis", 1, new VerseRange(5, 10));
+
+        await _dbContext.Lessons.AddRangeAsync(lesson1, lesson2);
+        await _dbContext.SaveChangesAsync();
+        await SeedLessonPath();
+
+        var result = await _repository.GetAllLessonsAsync();
+        var lessons = result.ToList();
+        Assert.Equal(lesson2.BookChapterVerseRange(), lessons[0].BookChapterVerseRange());
+        Assert.Equal(lesson1.BookChapterVerseRange(), lessons[1].BookChapterVerseRange());
+    }
+
+
+}
---
+++ tests/ContextExplained.Tests/Infrastructure/SqliteDbFixture.cs
@@ -0,0 +1,38 @@
+﻿using ContextExplained.Infrastructure.Adapters;
+using ContextExplained.Infrastructure.DataSeeder;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace ContextExplained.IntegrationTests;
+
+public class SqliteDbFixture : IAsyncLifetime
+{
+    public ApplicationDbContext Context { get; private set; }
+    private readonly SqliteConnection _connection;
+
+    public SqliteDbFixture()
+    {
+        _connection = new SqliteConnection("Filename=:memory:");
+        _connection.Open();
+
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseSqlite(_connection)
+            .Options;
+
+        Context = new ApplicationDbContext(options);
+        Context.Database.OpenConnection();
+        Context.Database.EnsureCreated();
+    }
+    public Task InitializeAsync() => Task.CompletedTask;
+
+    public async Task SeedAsync(Func<ApplicationDbContext, Task> seeder)
+    {
+        await seeder(Context);
+    }
+
+    public async Task DisposeAsync()
+    {
+        await Context.DisposeAsync();
+    }
+}
